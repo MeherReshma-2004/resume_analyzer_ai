944e5f57171481ca8020788f7fa30a9cf6b597eb
Potnuru Meher Reshma <pmeherreshma@gmail.com>
2026-03-31 11:56:18 +0530

    Final Commit

---
+++ pdf.py
@@ -0,0 +1,13 @@
+from pypdf import PdfReader
+
+def extractpdf(pdf_doc):# module to load and extract text from the PDF
+    try:
+        pdf=PdfReader(pdf_doc)  
+        raw_text=''
+        for index,page in enumerate(pdf.page):  # by index, page from the loaded doc
+            text=page.extract_text()# from page it will extract text
+            if text:
+                raw_text += text
+        return raw_text
+    except Exception as e:
+        return f'Error in reading the pdf' 
